578419ecf8d6b984f1ad219de7d1e89ad30fb912
CopBoat <214928991+CopBoat@users.noreply.github.com>
2025-09-14 08:15:36 -0700

    added menu

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,8 @@ int main( int argc, char* args[] )
     std::srand(static_cast<unsigned int>(time(0)));
     std::srand(static_cast<unsigned int>(std::time(0)));
 
+    bool playing = false;
+
     //Initialize
     if( init(chooseWindowTitle()) == false ) //initialize SDL Components
     {
@@ -35,6 +37,9 @@ int main( int argc, char* args[] )
         SDL_Log( "Unable to load media!\n" );
         exitCode = 1;
     }
+
+    
+
     else //if everything initialized fine
     {
         bool quit{ false }; //The quit flag
@@ -56,51 +61,76 @@ int main( int argc, char* args[] )
                     quit = true; //End the main loop
                 }
 
-                if (e.type == SDL_EVENT_KEY_DOWN) //Handle keyboard inputs
+                if (currentState == GameState::MENU) {
+                    //handleMenuEvent(e, quit, currentState);
+                    if (e.type == SDL_EVENT_KEY_DOWN) {
+                        if (e.key.key == SDLK_1) currentState = GameState::PLAYING;
+                        if (e.key.key == SDLK_2) currentState = GameState::OPTIONS;
+                    }
+                    //continue; // Skip the rest of the loop while in menu
+                } else if (currentState == GameState::OPTIONS) {
+                    //handleOptionsEvent(e, currentState);
+                    if (e.type == SDL_EVENT_KEY_DOWN && e.key.key == SDLK_ESCAPE)
+                        currentState = GameState::MENU;
+                    //continue; // Skip the rest of the loop while in options
+                }
+
+                // Render based on state
+                if (currentState == GameState::MENU) { renderMenu(); }
+                else if (currentState == GameState::OPTIONS) { renderOptions(); }
+                else 
                 {
-                    switch (e.key.key)
+                    playing = true;
+                    if (e.type == SDL_EVENT_KEY_DOWN) //Handle keyboard inputs
                     {
-                        case SDLK_LEFT: action = InputAction::MoveLeft;  break;
-                        case SDLK_RIGHT: action = InputAction::MoveRight; break;
-                        case SDLK_UP: action = InputAction::RotateClockwise; break;
-                        case SDLK_DOWN: action = InputAction::SoftDrop;  break;
-                        case SDLK_H: action = InputAction::Hold;      break;
-                        case SDLK_SPACE: action = InputAction::HardDrop;  break;
-                        case SDLK_ESCAPE: action = InputAction::Pause; break;
-                        default: break;
-                    }
-                }
-
-                if (e.type == SDL_EVENT_GAMEPAD_BUTTON_DOWN) //Handle gamepad inputs
-                {
-                    switch (e.gbutton.button) {
-                        case SDL_GAMEPAD_BUTTON_DPAD_LEFT:     action = InputAction::MoveLeft;               break;
-                        case SDL_GAMEPAD_BUTTON_DPAD_RIGHT:    action = InputAction::MoveRight;              break;
-                        case SDL_GAMEPAD_BUTTON_WEST:          action = InputAction::RotateClockwise;        break;
-                        case SDL_GAMEPAD_BUTTON_EAST:          action = InputAction::RotateCounterClockwise; break;
-                        case SDL_GAMEPAD_BUTTON_DPAD_DOWN:     action = InputAction::SoftDrop;               break;
-                        case SDL_GAMEPAD_BUTTON_SOUTH:         action = InputAction::HardDrop;               break;
-                        case SDL_GAMEPAD_BUTTON_LEFT_SHOULDER: action = InputAction::Hold;                   break;
-                        case SDL_GAMEPAD_BUTTON_START:         action = InputAction::Pause;                  break;
-                        default: break;
-                    }
-                }
-
-                if (e.type == SDL_EVENT_GAMEPAD_AXIS_MOTION) //Handle gamepad axis motion
-                {
-                    if (e.gaxis.axis == SDL_GAMEPAD_AXIS_LEFTX) {
-                        if (e.gaxis.value < -8000) {
-                            action = InputAction::MoveLeft;
-                        } else if (e.gaxis.value > 8000) {
-                            action = InputAction::MoveRight;
-                        }
-                    } else if (e.gaxis.axis == SDL_GAMEPAD_AXIS_LEFTY) {
-                        if (e.gaxis.value > 8000) {
-                            action = InputAction::SoftDrop;
+                        switch (e.key.key)
+                        {
+                            case SDLK_LEFT: action = InputAction::MoveLeft;  break;
+                            case SDLK_RIGHT: action = InputAction::MoveRight; break;
+                            case SDLK_UP: action = InputAction::RotateClockwise; break;
+                            case SDLK_DOWN: action = InputAction::SoftDrop;  break;
+                            case SDLK_H: action = InputAction::Hold;      break;
+                            case SDLK_SPACE: action = InputAction::HardDrop;  break;
+                            case SDLK_ESCAPE: action = InputAction::Pause; break;
+                            default: break;
                         }
                     }
+
+                    if (e.type == SDL_EVENT_GAMEPAD_BUTTON_DOWN) //Handle gamepad inputs
+                    {
+                        switch (e.gbutton.button) {
+                            case SDL_GAMEPAD_BUTTON_DPAD_LEFT:     action = InputAction::MoveLeft;               break;
+                            case SDL_GAMEPAD_BUTTON_DPAD_RIGHT:    action = InputAction::MoveRight;              break;
+                            case SDL_GAMEPAD_BUTTON_WEST:          action = InputAction::RotateClockwise;        break;
+                            case SDL_GAMEPAD_BUTTON_EAST:          action = InputAction::RotateCounterClockwise; break;
+                            case SDL_GAMEPAD_BUTTON_DPAD_DOWN:     action = InputAction::SoftDrop;               break;
+                            case SDL_GAMEPAD_BUTTON_SOUTH:         action = InputAction::HardDrop;               break;
+                            case SDL_GAMEPAD_BUTTON_LEFT_SHOULDER: action = InputAction::Hold;                   break;
+                            case SDL_GAMEPAD_BUTTON_START:         action = InputAction::Pause;                  break;
+                            default: break;
+                        }
+                    }
+
+                    if (e.type == SDL_EVENT_GAMEPAD_AXIS_MOTION) //Handle gamepad axis motion
+                    {
+                        if (e.gaxis.axis == SDL_GAMEPAD_AXIS_LEFTX) {
+                            if (e.gaxis.value < -8000) {
+                                action = InputAction::MoveLeft;
+                            } else if (e.gaxis.value > 8000) {
+                                action = InputAction::MoveRight;
+                            }
+                        } else if (e.gaxis.axis == SDL_GAMEPAD_AXIS_LEFTY) {
+                            if (e.gaxis.value > 8000) {
+                                action = InputAction::SoftDrop;
+                            }
+                        }
+                    }
                 }
+                
+               
             }
+
+            if (!playing) continue; // Skip the rest of the loop if not playing
 
             switch (action) { //handle input actions
                 case InputAction::MoveLeft: { moveLeft(); break; }
